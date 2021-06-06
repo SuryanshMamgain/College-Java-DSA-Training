0e922e38bd1c745c1d6a0d8b55c0476975c95ea1
Suryansh Mamgain <69207593+SuryanshMamgain@users.noreply.github.com>
2021-06-06 21:45:27 +0530

    Day-2

--- java day-3/salary caculator updated/TestEmployee.java
+++
@@ -1,8 +0,0 @@
-public class TestEmployee{
-	public static void main(String[] args) {
-		Employee employee=new Employee(90000.0f,"Ram Krishna",162182,"ramkrishna@gmail.com");
-		employee.calculate();
-		employee.calculateTax();
-		employee.show();
-	}
-}
--- java day-3/student report card/TestStudent.java
+++
@@ -1,8 +0,0 @@
-public class TestStudent {
-    public static void main(String [] args)
-    {
-    Student student=new Student("Suryansh Mamgain","987662383","suryanshmamgain@gmail.com",20,"Village=Salan gaon,PO-Bhagwantpur,Dehradun");
-    student.show();
-	student.calculate();
-}
-}
