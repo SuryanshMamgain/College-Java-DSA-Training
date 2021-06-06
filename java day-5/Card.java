141b9030609a88603fc4f3564c20bca2308cf165
Suryansh Mamgain <69207593+SuryanshMamgain@users.noreply.github.com>
2021-06-06 21:19:53 +0530

    Day-5

---
+++ java day-5/Card.java
@@ -0,0 +1,581 @@
+
+public interface Card {
+	void method1();
+	void method2();
+	void method3();
+	void method4();
+	void method5();
+	void method6();
+	void method7();
+	void method8();
+	void method9();
+	void method10();
+	void method11();
+	void method12();
+	void method13();
+	void method14();
+	void method15();
+	void method16();
+	void method17();
+	void method18();
+	void method19();
+	void method20();
+	void method21();
+	void method22();
+	void method23();
+	void method24();
+	void method25();
+	void method26();
+	void method27();
+	void method28();
+	void method29();
+	void method30();
+}
+abstract class Cardadapter implements Card{
+
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method4() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method5() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method6() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method7() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method8() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method9() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method10() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method11() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method12() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method13() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method14() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method15() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method16() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method17() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method18() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void method19() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method20() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method21() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method22() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method23() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method24() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method25() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method26() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method27() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method28() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method29() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void method30() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+class SilverCard extends Cardadapter{
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+		super.method1();
+	}
+
+	@Override
+	public void method2() {
+		// TODO Auto-generated method stub
+		super.method2();
+	}
+
+	@Override
+	public void method3() {
+		// TODO Auto-generated method stub
+		super.method3();
+	}
+
+	@Override
+	public void method4() {
+		// TODO Auto-generated method stub
+		super.method4();
+	}
+
+	@Override
+	public void method5() {
+		// TODO Auto-generated method stub
+		super.method5();
+	}
+
+	@Override
+	public void method6() {
+		// TODO Auto-generated method stub
+		super.method6();
+	}
+
+	@Override
+	public void method7() {
+		// TODO Auto-generated method stub
+		super.method7();
+	}
+
+	@Override
+	public void method8() {
+		// TODO Auto-generated method stub
+		super.method8();
+	}
+
+	@Override
+	public void method9() {
+		// TODO Auto-generated method stub
+		super.method9();
+	}
+
+	@Override
+	public void method10() {
+		// TODO Auto-generated method stub
+		super.method10();
+	}
+}
+class GoldCard extends Cardadapter{
+	@Override
+	public void method11() {
+		// TODO Auto-generated method stub
+		super.method11();
+	}
+
+	@Override
+	public void method12() {
+		// TODO Auto-generated method stub
+		super.method12();
+	}
+
+	@Override
+	public void method13() {
+		// TODO Auto-generated method stub
+		super.method13();
+	}
+
+	@Override
+	public void method14() {
+		// TODO Auto-generated method stub
+		super.method14();
+	}
+
+	@Override
+	public void method15() {
+		// TODO Auto-generated method stub
+		super.method15();
+	}
+
+	@Override
+	public void method16() {
+		// TODO Auto-generated method stub
+		super.method16();
+	}
+
+	@Override
+	public void method17() {
+		// TODO Auto-generated method stub
+		super.method17();
+	}
+
+	@Override
+	public void method18() {
+		// TODO Auto-generated method stub
+		super.method18();
+	}
+	
+	@Override
+	public void method19() {
+		// TODO Auto-generated method stub
+		super.method19();
+	}
+
+	@Override
+	public void method20() {
+		// TODO Auto-generated method stub
+		super.method20();
+		
+	}
+
+	@Override
+	public void method21() {
+		// TODO Auto-generated method stub
+		super.method21();
+	}
+
+	@Override
+	public void method22() {
+		// TODO Auto-generated method stub
+		super.method22();
+	}
+
+	@Override
+	public void method23() {
+		// TODO Auto-generated method stub
+		super.method23();
+	}
+
+	@Override
+	public void method24() {
+		// TODO Auto-generated method stub
+		super.method24();
+	}
+
+	@Override
+	public void method25() {
+		// TODO Auto-generated method stub
+		super.method25();
+	}
+
+	@Override
+	public void method26() {
+		// TODO Auto-generated method stub
+		super.method26();
+	}
+
+	@Override
+	public void method27() {
+		// TODO Auto-generated method stub
+		super.method27();
+	}
+
+	@Override
+	public void method28() {
+		// TODO Auto-generated method stub
+		super.method28();
+	}
+
+	@Override
+	public void method29() {
+		// TODO Auto-generated method stub
+		super.method29();
+	}
+
+	@Override
+	public void method30() {
+		// TODO Auto-generated method stub
+		super.method30();
+	}
+}
+class DiamondCard extends Cardadapter{
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+		super.method1();
+	}
+
+	@Override
+	public void method2() {
+		// TODO Auto-generated method stub
+		super.method2();
+	}
+
+	@Override
+	public void method3() {
+		// TODO Auto-generated method stub
+		super.method3();
+	}
+
+	@Override
+	public void method4() {
+		// TODO Auto-generated method stub
+		super.method4();
+	}
+
+	@Override
+	public void method5() {
+		// TODO Auto-generated method stub
+		super.method5();
+	}
+
+	@Override
+	public void method6() {
+		// TODO Auto-generated method stub
+		super.method6();
+	}
+
+	@Override
+	public void method7() {
+		// TODO Auto-generated method stub
+		super.method7();
+	}
+
+	@Override
+	public void method8() {
+		// TODO Auto-generated method stub
+		super.method8();
+	}
+
+	@Override
+	public void method9() {
+		// TODO Auto-generated method stub
+		super.method9();
+	}
+
+	@Override
+	public void method10() {
+		// TODO Auto-generated method stub
+		super.method10();
+	}
+
+	@Override
+	public void method11() {
+		// TODO Auto-generated method stub
+		super.method11();
+	}
+
+	@Override
+	public void method12() {
+		// TODO Auto-generated method stub
+		super.method12();
+	}
+
+	@Override
+	public void method13() {
+		// TODO Auto-generated method stub
+		super.method13();
+	}
+
+	@Override
+	public void method14() {
+		// TODO Auto-generated method stub
+		super.method14();
+	}
+
+	@Override
+	public void method15() {
+		// TODO Auto-generated method stub
+		super.method15();
+	}
+
+	@Override
+	public void method16() {
+		// TODO Auto-generated method stub
+		super.method16();
+	}
+
+	@Override
+	public void method17() {
+		// TODO Auto-generated method stub
+		super.method17();
+	}
+
+	@Override
+	public void method18() {
+		// TODO Auto-generated method stub
+		super.method18();
+	}
+	
+	@Override
+	public void method19() {
+		// TODO Auto-generated method stub
+		super.method19();
+	}
+
+	@Override
+	public void method20() {
+		// TODO Auto-generated method stub
+		super.method20();
+		
+	}
+
+	@Override
+	public void method21() {
+		// TODO Auto-generated method stub
+		super.method21();
+	}
+
+	@Override
+	public void method22() {
+		// TODO Auto-generated method stub
+		super.method22();
+	}
+
+	@Override
+	public void method23() {
+		// TODO Auto-generated method stub
+		super.method23();
+	}
+
+	@Override
+	public void method24() {
+		// TODO Auto-generated method stub
+		super.method24();
+	}
+
+	@Override
+	public void method25() {
+		// TODO Auto-generated method stub
+		super.method25();
+	}
+
+	@Override
+	public void method26() {
+		// TODO Auto-generated method stub
+		super.method26();
+	}
+
+	@Override
+	public void method27() {
+		// TODO Auto-generated method stub
+		super.method27();
+	}
+
+	@Override
+	public void method28() {
+		// TODO Auto-generated method stub
+		super.method28();
+	}
+
+	@Override
+	public void method29() {
+		// TODO Auto-generated method stub
+		super.method29();
+	}
+
+	@Override
+	public void method30() {
+		// TODO Auto-generated method stub
+		super.method30();
+	}
+}
