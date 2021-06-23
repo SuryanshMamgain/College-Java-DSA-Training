65222c44d89a3a59ca6ea6a1298ef98cc24accc7
Suryansh Mamgain <69207593+SuryanshMamgain@users.noreply.github.com>
2021-06-24 01:15:10 +0530

    recursion

---
+++ DSA_Practice_Questions/DAY-9/SubSequence.java
@@ -0,0 +1,30 @@
+//Input-"Suru"
+//Output-[ , S , u , Su , r , Sr , ur , Sur , u , Su , uu , Suu , ru , Sru , uru , Suru ]
+
+import java.util.ArrayList;
+
+public class SubSequence {
+	static ArrayList<String> getSubSequences(String str){
+		if(str.length()==0) {
+			ArrayList<String> emptyString=new ArrayList<>();
+			emptyString.add(" ");
+			return emptyString;
+		}
+		char currentChar=str.charAt(0);
+		String remainingString=str.substring(1);
+		ArrayList<String> result=new ArrayList<>();
+		ArrayList<String> temp=getSubSequences(remainingString);
+		for(String ele:temp) {
+			result.add(ele);
+			result.add(currentChar+ele);
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> results= getSubSequences("Suru");
+		System.out.println(results);
+	}
+
+}
