20c3e8f2dc39c68a937fbe76ae139e001b8a0d4f
Suryansh Mamgain <69207593+SuryanshMamgain@users.noreply.github.com>
2021-07-05 20:48:30 +0530

    dp

--- DSA_Practice_Questions/DAY-18/DP QUES/CoinChange.java
+++ DSA_Practice_Questions/DAY-18/DP QUES/CoinChange.java
@@ -1,5 +1,6 @@
 
 public class CoinChange {
+	//recursion approach to print all the combinations//
 	static void coinchange(int[] coins,int index,int target,String result) {
 		if(target==0) {
 			System.out.println(result);
@@ -11,12 +12,27 @@ public class CoinChange {
 			}
 		}
 	}
-
+	
+//DP approach to count coins Combinations//
+	
+	static int coinchangecountTabu(int[] coins,int targetamount) {
+		int cache[]=new int[targetamount+1];
+		cache[0]=1;
+		for(int coin:coins) {
+			for(int i=1;i<cache.length;i++) {
+				if(i>=coin) {
+				cache[i]+=cache[i-coin];
+				}
+			}	
+		}
+		return cache[targetamount];
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int [] coins= {2,3,5,6};
 		int target=10;
 		coinchange(coins,0,target,"");
+		System.out.println("Count: "+coinchangecountTabu(coins, target));
 	}
 
 }
